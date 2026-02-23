b7ed88679f1c7b713b2e1fd1b72f502c99591bec
Imre Mehesz <imehesz@gmail.com>
2026-02-23 16:38:27 -0500

    Fixed Level 4 broken window

--- src/client/levels/Level4.ts
+++ src/client/levels/Level4.ts
@@ -137,7 +137,7 @@ export class Level4 extends BaseLevel {
 
     const screen = new THREE.Mesh(new THREE.BoxGeometry(0.14, 1.25, 1.25), this.screenMaterial);
     screen.position.set(2.8, 2.1, -4.82);
-    screen.rotation.y = 0;
+    screen.rotation.y = Math.PI / 2;
     this.worldRoot.add(screen);
 
     const frame = new THREE.Mesh(
